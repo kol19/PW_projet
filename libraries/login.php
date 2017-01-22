ae53f5df5d402eabe19f226241d9441fd91e1022
kol19 <maykolh@hotmail.com>
2017-01-22 23:56:46 +0100

    twitter upadte

--- libraries/login.php
+++ libraries/login.php
@@ -34,11 +34,11 @@ if (!empty($_GET['msjctrl'])) {
 
 <div class="form-group">
    <label class="sr-only" for="exampleInputEmail2">Email address</label>
-   <input type="text" name="user" class="form-control" id="exampleInputEmail2" value="admin" placeholder="Email address" required>
+   <input type="text" name="user" class="form-control" id="exampleInputEmail2" value="" placeholder="Email address" required>
 </div>
 <div class="form-group">
    <label class="sr-only" for="exampleInputPassword2">Password</label>
-   <input type="password" name = "password" class="form-control" id="exampleInputPassword2" placeholder="Password" value="passadmin" required>
+   <input type="password" name = "password" class="form-control" id="exampleInputPassword2" placeholder="Password" value="" required>
 
 </div>
 <div class="form-group">
